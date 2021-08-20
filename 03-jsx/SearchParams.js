63736e1aedb258de59cb5f944d45b86c60ae7720
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-08-20 10:41:20 -0400

    setup for adopt me project

---
+++ 03-jsx/SearchParams.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SearchParams = () => {
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="location">
+                    Location
+                    <input id="location" value="Seattle, WA" placeholder="Location" />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchParams;
